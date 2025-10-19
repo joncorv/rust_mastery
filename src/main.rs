01018606e04ed3861ef3a2d4f6775a938d19c18c
Jonathan E. Corriveau <joncorv@gmail.com>
2025-10-18 23:55:54 -0700

    cleaned up importing code

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,9 @@
 mod code_practice;
 mod imports;
 
-use code_practice::cp_26_1_string_split::{string_split, test_string_split};
+// use code_practice::cp_26_1_string_split::test_string_split;
+use code_practice::*;
 
 fn main() {
-    test_string_split();
+    cp_26_1_string_split::test_string_split();
 }
