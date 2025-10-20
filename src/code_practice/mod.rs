e818bd0bf1d8b5721917e79f50557e351eb16818
Jonathan E. Corriveau <joncorv@gmail.com>
2025-10-19 22:46:58 -0700

    renamed palindrome sentence

--- src/code_practice/mod.rs
+++ src/code_practice/mod.rs
@@ -1,4 +1,4 @@
 pub mod cp_26_1_string_split;
 pub mod cp_27_1_valid_palindrome;
-pub mod cp_27_1_valid_palindrome_whitespace;
+pub mod cp_27_1_valid_palindrome_sentence;
 pub mod cp_27_3_array_intersection;
--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,6 @@ use code_practice::*;
 fn main() {
     // cp_26_1_string_split::test_string_split();
     // cp_27_1_valid_palindrome::test_valid_palindrome();
-    // cp_27_1_valid_palindrome_whitespace::test_valid_palindrome_whitespace();
-    cp_27_3_array_intersection::test_array_intersection();
+    cp_27_1_valid_palindrome_sentence::test_valid_palindrome_sentence();
+    // cp_27_3_array_intersection::test_array_intersection();
 }
