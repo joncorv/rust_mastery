5721e7015824f766dfc68196e670fdd825bc6178
Jonathan E. Corriveau <joncorv@gmail.com>
2025-10-19 20:36:46 -0700

    finished valid palanedrome whitespace with proper error handling

--- src/code_practice/cp_27_1_valid_palindrome_whitespace.rs
+++ src/code_practice/cp_27_1_valid_palindrome_whitespace.rs
@@ -1,6 +1,10 @@
 #![allow(dead_code, unused_imports, unused_variables)]
 
 pub fn valid_palindrome_whitespace(s: String) -> bool {
+    if s.is_empty() {
+        return true;
+    }
+
     let mut start: usize = 0;
     let mut end: usize = s.len() - 1;
     let s: Vec<char> = s.chars().collect();
@@ -17,16 +21,22 @@ pub fn valid_palindrome_whitespace(s: String) -> bool {
                     start += 1;
                     break 'start;
                 }
+            } else {
+                return true;
             }
         }
 
         'end: loop {
-            if s[end] == ' ' {
-                end -= 1;
+            if let Some(val) = s.get(end) {
+                if *val == ' ' {
+                    end -= 1;
+                } else {
+                    end_char = val;
+                    end -= 1;
+                    break 'end;
+                }
             } else {
-                end_char = s[end];
-                end -= 1;
-                break 'end;
+                return true;
             }
         }
 
@@ -47,12 +57,22 @@ pub fn test_valid_palindrome_whitespace() {
     println!("Result: {test:?}");
 
     let test_string = "ra cecar";
-    println!("Testing: {test_string}. This should return false");
+    println!("Testing: {test_string}. This should return true");
     let test = valid_palindrome_whitespace(test_string.to_string());
     println!("Result: {test:?}");
 
-    let test_string = "abcdcba";
+    let test_string = " a bc dcba  ";
     println!("Testing: {test_string}. This should return true");
+    let test = valid_palindrome_whitespace(test_string.to_string());
+    println!("Result: {test:?}");
+
+    let test_string = "";
+    println!("Testing: {test_string}. This should return true");
+    let test = valid_palindrome_whitespace(test_string.to_string());
+    println!("Result: {test:?}");
+
+    let test_string = "    a !   ";
+    println!("Testing: {test_string}. This should return false");
     let test = valid_palindrome_whitespace(test_string.to_string());
     println!("Result: {test:?}");
 }
