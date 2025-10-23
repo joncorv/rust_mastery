bc29bd20db90a5f05aa58b2d0a9343c78a23edf2
Jonathan Corriveau <joncorv@gmail.com>
2025-10-23 14:30:34 -0700

    fixed bug in shift_word_to_back function

--- src/code_practice/cp_27_18_shift_word_to_back.rs
+++ src/code_practice/cp_27_18_shift_word_to_back.rs
@@ -10,22 +10,30 @@ pub fn shift_word_to_back(arr: Vec<char>, word: String) -> Vec<char> {
     let word: Vec<char> = word.chars().collect();
     let len_arr = arr.len();
     let len_word = word.len();
+    let default_char: char = ' ';
 
-    'main_loop: loop {
-        if arr[seeker] != word[word_search_index] {
-            (arr[seeker], arr[writer]) = (arr[writer], arr[seeker]);
-            if word_search_index < word.len() - 1 {
-                seeker += 1;
-                writer += 1;
-                word_search_index += 1;
-            } else {
-                break 'main_loop;
-            }
+    while seeker < len_arr {
+        // get test_char safely because you'll run out of bounds
+        let test_char: char;
+        if let Some(ch) = word.get(word_search_index) {
+            test_char = *ch;
         } else {
-            seeker += 1
+            test_char = default_char;
+        }
+
+        // if seeker finds test char, move seeker and word_search index up
+        if arr[seeker] == test_char {
+            word_search_index += 1;
+            seeker += 1;
+        } else {
+            // or else swap and iteratate seeker + writer
+            arr.swap(seeker, writer);
+            seeker += 1;
+            writer += 1
         }
     }
 
+    // clean up and write the given word to the end of the array
     for (index, letter) in word.iter().enumerate() {
         arr[len_arr - len_word + index] = *letter;
     }
@@ -42,8 +50,8 @@ pub fn test_shift_word_to_back() {
     println!("chars: {arr:?}, word: {word:?}");
     println!("result: {test_result:?}");
 
-    let arr = vec!['a', 'f', 'b', 'u', 'c', 'c', 'd', 'k'];
-    let word = "abc".to_string();
+    let arr = vec!['a', 'w', 'b', 'x', 'c', 'y', 'd', 'z'];
+    let word = "abcd".to_string();
     let test_result = shift_word_to_back(arr.clone(), word.clone());
     println!("chars: {arr:?}, word: {word:?}");
     println!("result: {test_result:?}");
