ca6ccde7108d1fa968e874c1c7a829173e7b3fb1
joncorv <joncorv@gmail.com>
2025-11-07 13:09:25 -0800

    add multi_account_cheating

--- src/main.rs
+++ src/main.rs
@@ -21,5 +21,6 @@ fn main() {
     // cp_29_04_two_array_two_sum::test_two_array_two_sum();
     // cp_29_05_target_count_divisible_by_k::test_target_count_divisible_by_k();
     // cp_30_01_account_sharing::test_account_sharing();
-    cp_30_03_most_frequent_octet::test_most_frequent_octet();
+    // cp_30_03_most_frequent_octet::test_most_frequent_octet();
+    cp_30_04_multi_account_cheating::test_multi_account_cheating();
 }
