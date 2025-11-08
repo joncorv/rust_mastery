4b2f0fee194158e4dcd68dcf2b97b94320a910e0
Jonathan E. Corriveau <joncorv@gmail.com>
2025-11-08 15:59:28 -0800

    wip cheater detection

---
+++ src/code_practice/cp_30_08_cheater_detection.rs
@@ -0,0 +1,6 @@
+// cheater detection
+// given an array of answers
+// k is number of questions in the exam
+// possible answers are "a", "b", "c", "d"
+
+// honestly this one is just annoying i'm going to skip it for now
