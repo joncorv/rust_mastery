758e595b4daa6f8de43611e70825ac5623e44aa7
Jonathan E. Corriveau <joncorv@gmail.com>
2026-06-17 21:55:01 -0700

    graph maze completed

--- src/playground/pg_05_graph_maze.rs
+++ src/playground/pg_05_graph_maze.rs
@@ -10,15 +10,15 @@ fn get_valid_neighbors(maze: &Vec<Vec<i8>>, cell: (i8, i8)) -> Vec<(i8, i8)> {
     let mut valid_neighbors = Vec::new();
     let dirs: Vec<(i8, i8)> = vec![(-1, 0), (1, 0), (0, 1), (0, -1)];
     for dir in dirs {
-        let mut in_bounds = false;
-
         let test_cell = (dir.0 + cell.0, dir.1 + cell.1);
         let in_bounds = test_cell.0 >= 0 && test_cell.0 < (maze.len() as i8) && test_cell.1 >= 0 && test_cell.1 < (maze[0].len() as i8);
         // println!("test cell {test_cell:?} is {in_bounds}");
         if in_bounds {
             let valid_cell = maze[test_cell.0 as usize][test_cell.1 as usize] == 1;
-            // println!("in-bounds cell {test_cell:?} is valid? {valid_cell}");
-            valid_neighbors.push(test_cell);
+            if valid_cell {
+                // println!("cell: {cell:?} and test_cell: {test_cell:?} is valid? {valid_cell}");
+                valid_neighbors.push(test_cell);
+            }
         }
     }
     valid_neighbors
@@ -39,12 +39,13 @@ pub fn graph_maze(maze: Vec<Vec<i8>>, start: (i8, i8), end: (i8, i8)) -> Context
         if context.cell == *end {
             context.success = true;
             context.success_path = context.path.clone();
+            // println!("success found!");
             return;
         }
 
         // find valid neighbors over current cell
         let valid_neighbors = get_valid_neighbors(maze, context.cell);
-        println!("valid neighbors: {:?}", valid_neighbors.clone());
+        // println!("valid neighbors: {:?}", valid_neighbors.clone());
 
         // recursion over valid neighbors
         for neighbor in valid_neighbors {
@@ -66,40 +67,42 @@ pub fn graph_maze(maze: Vec<Vec<i8>>, start: (i8, i8), end: (i8, i8)) -> Context
 
 pub fn test_graph_maze() {
     let maze: Vec<Vec<i8>> = vec![
-        vec![1, 1, 1, 0, 0, 0, 0, 1, 0, 1, 1],
-        vec![0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0],
-        vec![0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0],
-        vec![0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0],
-        vec![0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
-        vec![0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
-        vec![0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
-        vec![0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
-        vec![0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
-        vec![0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
-        vec![0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 1],
+        vec![1, 1, 1, 1, 0, 1, 0, 0, 0, 0],
+        vec![0, 0, 0, 1, 0, 1, 0, 0, 0, 0],
+        vec![0, 0, 0, 1, 0, 1, 0, 1, 0, 0],
+        vec![0, 0, 0, 1, 1, 1, 1, 1, 1, 0],
+        vec![0, 0, 0, 1, 0, 0, 0, 1, 1, 0],
+        vec![0, 0, 1, 1, 0, 0, 0, 0, 1, 0],
+        vec![1, 1, 1, 0, 0, 0, 1, 1, 1, 0],
+        vec![0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+        vec![0, 0, 0, 0, 0, 0, 1, 1, 0, 0],
+        vec![0, 0, 0, 0, 0, 0, 0, 1, 1, 1],
     ];
     let start = (0, 0);
-    let end = (10, 10);
+    let end = (9, 9);
+    println!("\nProessing has begun");
     let result = graph_maze(maze.clone(), start, end);
-    println!("graph maze result num success path: {:?}", result.success_path.len());
 
-    // let mut char_vecs = vec![vec!['-'; 11]; 11];
-
-    let mut result_view = ma
-
-    for res in result.success_path {
-        char_vecs[res.0 as usize][res.1 as usize] = 'x';
+    if result.success {
+        println!("HOORAY, We found a path out of the maze!!! 😃")
+    } else {
+        println!("CRAP. We couldn't find a path out of the maze 😢")
     }
+    // println!("graph maze result num success path: {:?}", result.success_path);
 
-    for res in result.success_path {
-        char_vecs[res.0 as usize][res.1 as usize] = 'x';
-    }
-
+    // show question
+    println!("\nOriginal Question:");
     for row in maze.clone() {
         println!("{row:?}");
     }
 
-    for row in char_vecs {
+    // show result
+    println!("\nSuccessful Path:");
+    let mut result_view = maze.clone();
+    for res in result.success_path {
+        result_view[res.0 as usize][res.1 as usize] = 2;
+    }
+    for row in result_view {
         println!("{row:?}");
     }
 }
